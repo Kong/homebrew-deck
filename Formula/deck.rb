db79b322d6531baba4e156161623462559ff4950
Harry Bagdi <harrybagdi@gmail.com>
2019-12-07 17:20:33 -0800

    release v0.7.0

--- Formula/deck.rb
+++ Formula/deck.rb
@@ -2,15 +2,15 @@
 class Deck < Formula
   desc "Declarative configuration for Kong"
   homepage "https://github.com/hbagdi/deck"
-  version "0.6.2"
+  version "0.7.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hbagdi/deck/releases/download/v0.6.2/deck_0.6.2_darwin_amd64.tar.gz"
-    sha256 "35173e2cf34ce6831464d90d7a3a76fd73a1d1686e115ea975047791c08b3807"
+    url "https://github.com/hbagdi/deck/releases/download/v0.7.0/deck_0.7.0_darwin_amd64.tar.gz"
+    sha256 "a7f368fa03129108050bc8c2bde46a6c9f9ce02d6daaf9a83d8c010e9b5a877c"
   elsif OS.linux?
-    url "https://github.com/hbagdi/deck/releases/download/v0.6.2/deck_0.6.2_linux_amd64.tar.gz"
-    sha256 "aeca5cfe112ed2d57cf97c5e25bde12f4e48e0fc403cd347123ae9f68d451ec4"
+    url "https://github.com/hbagdi/deck/releases/download/v0.7.0/deck_0.7.0_linux_amd64.tar.gz"
+    sha256 "753fcad53dc0f38146266b326ac060df564c4607c1b323d0d7afc6a3b94e0e90"
   end
 
   def install
