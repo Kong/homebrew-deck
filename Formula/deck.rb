94dd28333afcee8cc7c7d6a07229ba977f6e31d5
rajkong <rajan.mahadevan@konghq.com>
2021-12-15 09:46:25 -0800

    v1.10.0 release

--- Formula/deck.rb
+++ Formula/deck.rb
@@ -5,32 +5,44 @@
 class Deck < Formula
   desc "Declarative configuration for Kong"
   homepage "https://github.com/kong/deck"
-  version "1.9.0"
+  version "1.10.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Kong/deck/releases/download/v1.9.0/deck_1.9.0_darwin_amd64.tar.gz"
-      sha256 "a4817c817c27ed57ba605eccf58d2b1f81826c235afa215aae21dc2475a19be1"
+      url "https://github.com/Kong/deck/releases/download/v1.10.0/deck_1.10.0_darwin_amd64.tar.gz"
+      sha256 "5050ce18574b68f1a7e16165ce53fb40218c32086879509d8752ca81de2cf796"
+
+      def install
+        bin.install "deck"
+      end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Kong/deck/releases/download/v1.9.0/deck_1.9.0_darwin_arm64.tar.gz"
-      sha256 "0e2104416da1c0f1891adec90af719b2384abc4e6f6738676875e085d8e101d0"
+      url "https://github.com/Kong/deck/releases/download/v1.10.0/deck_1.10.0_darwin_arm64.tar.gz"
+      sha256 "feae3b47973892d6d846de73f943d4db406c2df5bdf53bd61645df27b2843455"
+
+      def install
+        bin.install "deck"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/Kong/deck/releases/download/v1.9.0/deck_1.9.0_linux_amd64.tar.gz"
-      sha256 "bcab2d8f8b346134ee82674a617c08d9cda587a84358f15f5565695eab22d181"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Kong/deck/releases/download/v1.9.0/deck_1.9.0_linux_arm64.tar.gz"
-      sha256 "2456790a06d809e17e99c6a4314acc45b4ca10fe7a5fc1e4bdc7a58e42ae5913"
-    end
-  end
+      url "https://github.com/Kong/deck/releases/download/v1.10.0/deck_1.10.0_linux_arm64.tar.gz"
+      sha256 "d75745bdaa332971814ded23903ad5325a18ae83605b9331cdc19b67f12db67f"
 
-  def install
-    bin.install "deck"
+      def install
+        bin.install "deck"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/Kong/deck/releases/download/v1.10.0/deck_1.10.0_linux_amd64.tar.gz"
+      sha256 "01b8f93a634d4c50e8d6f21be85b7419a117b4643c67b94e8c13643819a22213"
+
+      def install
+        bin.install "deck"
+      end
+    end
   end
 
   test do
